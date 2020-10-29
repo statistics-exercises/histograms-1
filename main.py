850b61a959bb235a5946f074bfadb563f955d36c
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-29 20:37:06 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def bernoulli(p) : 
+  # Your code to generate a bernoulli random variable goes here
+  
+def repeated_trials(n,p) :
+  nsuccess, nfail = 0, 0
+  # Your code to generate n bernoulli trials and to count the number of 
+  # successes and failures goes here.
+  
+  return nsuccess, nfail
+  
+print( repeated_trials(10,0.2) )
+print( repeated_trials(10,0.2) )
+print( repeated_trials(10,0.2) )
+print( repeated_trials(10,0.2) )

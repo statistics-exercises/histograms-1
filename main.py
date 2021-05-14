e3f9ac3b47c1ac672d232b36fac977ace5b9f283
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-14 10:43:50 +0100

    Added answers

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import numpy as np
 
 def bernoulli(p) : 
   # Your code to generate a bernoulli random variable goes here
-  
+  if np.random.uniform(0,1)<p : return 1
+  return 0 
+ 
 def repeated_trials(n,p) :
   nsuccess, nfail = 0, 0
   # Your code to generate n bernoulli trials and to count the number of 
   # successes and failures goes here.
-  
+  for i in range(n) : 
+      nsuccess = nsuccess + bernoulli(p)
+  nfail = n - nsuccess 
   return nsuccess, nfail
   
 print( repeated_trials(10,0.2) )
